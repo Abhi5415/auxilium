6b3c4865f5dfd54cc70f3066e4bb010acee81d6e
Shehryar Assad <sassad@huntsman-ve701-0948.eduroam.wlan.private.upenn.edu>
2019-09-06 21:51:48 -0400

    Add basic table

--- auxilium-client/src/App.js
+++ auxilium-client/src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { TransactionTable } from "./core-ui/transactions/TransactionTable";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 10px;
+`;
 
 export function App() {
-  return <div>Hello</div>;
+  return (
+    <Container>
+      Hello
+      <TransactionTable />
+    </Container>
+  );
 }
