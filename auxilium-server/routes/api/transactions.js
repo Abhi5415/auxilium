d37ea0db85cea495e3f7f00e7abbad768a1bd06c
Shehryar Assad <xshehryar@gmail.com>
2019-09-06 23:49:02 -0400

    Add endpoint for incoming/receiving for users

--- auxilium-server/routes/api/transactions.js
+++ auxilium-server/routes/api/transactions.js
@@ -2,17 +2,28 @@ const express = require("express");
 const router = express.Router();
 
 const { Transaction } = require("../../models/Transaction");
-const { User } = require("../../models/User");
 
-router.get("/test", (req, res) => res.json({ message: "Transactions works" }));
+router.get("/test", (_req, res) => res.json({ message: "Transactions works" }));
 
-router.get("/", async (req, res) => {
+router.get("/", async (_req, res) => {
   const transactions = Transaction.find();
   res.json(transactions);
 });
 
-router.post("/getTransactionsForUser", (req, res) => {
+router.post("/getOutgoingTransactionsForUser", (req, res) => {
   const { userId } = req;
+
+  Transaction.find({
+    initiatedId: userId
+  }).then(transactions => res.json(transactions));
+});
+
+router.post("/getIncomingTransactionsForUser", (req, res) => {
+  const { userId } = req;
+
+  Transaction.find({
+    destinationId: userId
+  }).then(transactions => res.json(transactions));
 });
 
 module.exports = router;
