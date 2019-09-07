a3532323d539b2acaf34552708846f776db2bb6a
Mufeez Amjad <zeefumdajma@outlook.com>
2019-09-07 00:24:28 -0700

    count how many coins are deposited

--- auxilium-atm/ir_sensor.py
+++ auxilium-atm/ir_sensor.py
@@ -1,22 +1,47 @@
 import RPi.GPIO as GPIO
+import time
+
+irPIN = 3
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(irPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 sensor_state = 0
 last_state = 0
 
+coinsDeposited = 0
+
+timeStarted = time.time()
+SECS_TO_WAIT = 5
+secsInactive = 0
+
 try:
+    f = open("deposit.txt", "w+")
+    f.write("pending")
+    f.close()
+
     while True:
-        sensor_state = GPIO.input(3)
+        sensor_state = GPIO.input(irPIN)
 
         if sensor_state and not last_state:
+            secsInactive = 0
             print('connected')
 
         if not sensor_state and last_state:
+            coinsDeposited += 1
+            secsInactive = 0
             print('broken')
 
+        if not sensor_state and not last_state:
+            time.sleep(1)
+            secsInactive += 1
+            if secsInactive is SECS_TO_WAIT:
+                 break
+
         last_state = sensor_state
+
+    f = open("deposit.txt","w+")
+    f.write(str(coinsDeposited))
 
 finally:
     GPIO.cleanup()
