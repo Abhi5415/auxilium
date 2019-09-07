2e3054c1acb5b28dc8fbf24076bfcc75c21c3c47
Abhinav <abhinav.b5415@gmail.com>
2019-09-07 04:12:26 -0400

    abhi5415 - completed stellar polling

--- auxilium-server/controllers/stellar.js
+++ auxilium-server/controllers/stellar.js
@@ -1,23 +1,25 @@
 const StellarSdk = require('stellar-sdk');
 StellarSdk.Network.useTestNetwork();
 const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
-const publicKey = 'GD4ILJJQNL6PYG5QXFL636CFD33BMSYJSGUXOP7LENIEVVZG5XQSOT7S';
-const secretString = 'SCUWWXUL42STRDXE3ZBIC3C6K5FGLNLDMIJ2C6AQHSENU6G5W4BFUM4Z';
+const validate = require('jsonschema').validate;
 
-// Public Key: GD4ILJJQNL6PYG5QXFL636CFD33BMSYJSGUXOP7LENIEVVZG5XQSOT7S
-// Secret Key: SCUWWXUL42STRDXE3ZBIC3C6K5FGLNLDMIJ2C6AQHSENU6G5W4BFUM4Z
+const publicKey = 'GAI6C5X6QK5KE3OXXIQAUZ33WCMCENCJ5VDYRKNZCR6LCYRM5PSQGFOG';
+const secretString = 'SALEHVAPOGW677KLEDFNQZ3WA7JSQCN2UJ37CUXHZOXIKHPEOOV264L7';
 
-// let transactionExample = {
-// 	u: '41614',
-// 	a: -1234,
-// };
+// Public Key: GAI6C5X6QK5KE3OXXIQAUZ33WCMCENCJ5VDYRKNZCR6LCYRM5PSQGFOG
+// Secret Key: SALEHVAPOGW677KLEDFNQZ3WA7JSQCN2UJ37CUXHZOXIKHPEOOV264L7
+
+let transactionExample = {
+	u: '41614',
+	a: -1234,
+};
 
 exports.submitTransaction = async payload => {
 	try {
 		const account = await server.loadAccount(publicKey);
 		const fee = await server.fetchBaseFee();
 		const memo = new StellarSdk.Memo.text(payload);
-		console.log(memo);
+		// console.log(memo);
 
 		const transaction = new StellarSdk.TransactionBuilder(account, {
 			fee,
@@ -39,20 +41,42 @@ exports.submitTransaction = async payload => {
 		transaction.sign(StellarSdk.Keypair.fromSecret(secretString));
 
 		const transactionResult = await server.submitTransaction(transaction);
+
 		console.log(transactionResult);
 	} catch (err) {
 		console.error(err);
 	}
 };
 
+// exports.submitTransaction(JSON.stringify(transactionExample));
+
 exports.getTransactions = async () => {
-	server
-		.transactions()
-		.forAccount(publicKey)
-		.call()
-		.then(function(r) {
-			console.log(r);
-		});
+	let history = [];
+
+	try {
+		r = await server
+			.transactions()
+			.limit(1)
+			.forAccount(publicKey)
+			.call();
+		while (r.records.length != 0) {
+			let record = r.records[0];
+
+			if (
+				record.hasOwnProperty('memo') &&
+				validate(transactionExample, { type: 'object' }).errors.length == 0
+			) {
+				let parsedRecord = JSON.parse(record.memo);
+				parsedRecord.r = record._links.self.href;
+				history.push(parsedRecord);
+			}
+
+			r = await r.next();
+		}
+		console.log(history);
+	} catch (e) {
+		console.log(e);
+	}
 };
 
 // exports.getTransactions();
