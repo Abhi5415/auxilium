5f95e27311cf83f4cf9a7dca8c728b2ffb2bfb9c
Abhinav <abhinav.b5415@gmail.com>
2019-09-06 22:48:46 -0400

    abhi5415 - Added memo to transaction

--- stellar-server/index.js
+++ stellar-server/index.js
@@ -7,14 +7,17 @@ const secretString = 'SCUWWXUL42STRDXE3ZBIC3C6K5FGLNLDMIJ2C6AQHSENU6G5W4BFUM4Z';
 // Public Key: GD4ILJJQNL6PYG5QXFL636CFD33BMSYJSGUXOP7LENIEVVZG5XQSOT7S
 // Secret Key: SCUWWXUL42STRDXE3ZBIC3C6K5FGLNLDMIJ2C6AQHSENU6G5W4BFUM4Z
 
-const submitTransaction = async payload => {
+exports.submitTransaction = async payload => {
 	try {
 		const account = await server.loadAccount(publicKey);
 		const fee = await server.fetchBaseFee();
 		const memo = new StellarSdk.Memo.text(payload);
 		console.log(memo);
 
-		const transaction = new StellarSdk.TransactionBuilder(account, { fee })
+		const transaction = new StellarSdk.TransactionBuilder(account, {
+			fee,
+			memo,
+		})
 			.addOperation(
 				// this operation funds the new account with XLM
 				StellarSdk.Operation.payment({
@@ -22,7 +25,6 @@ const submitTransaction = async payload => {
 						'GD4ILJJQNL6PYG5QXFL636CFD33BMSYJSGUXOP7LENIEVVZG5XQSOT7S',
 					asset: StellarSdk.Asset.native(),
 					amount: '0.0000001',
-					memo,
 				})
 			)
 			.setTimeout(30)
@@ -38,5 +40,3 @@ const submitTransaction = async payload => {
 		console.error(err);
 	}
 };
-
-submitTransaction('Test');
