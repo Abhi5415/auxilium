e9ea191061341583b067fa7dafc166275aca39d7
Shehryar Assad <xshehryar@gmail.com>
2019-09-07 15:17:15 -0400

    Add cors support

--- auxilium-server/server.js
+++ auxilium-server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const borrowers = require("./routes/api/borrowers");
 const transactions = require("./routes/api/transactions");
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 const db = require("./config/keys").mongoURI;
 
