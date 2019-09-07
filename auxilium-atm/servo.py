2336ad7f9bbe954dba1c17dffea9f319929cc538
Mufeez Amjad <zeefumdajma@outlook.com>
2019-09-07 01:05:44 -0700

    servo rotations n number of times

--- auxilium-atm/servo.py
+++ auxilium-atm/servo.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import sys
 
 servoPIN = 17
 GPIO.setmode(GPIO.BCM)
@@ -7,24 +8,20 @@ GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50)
 p.start(2.5)
+
+rotations = 0
+
+if len(sys.argv) > 1:
+  rotations = int(sys.argv[1])
+
 try:
-  while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
+  for i in range(rotations):
+     p.ChangeDutyCycle(12.5)
+     time.sleep(0.5)
+
+     p.ChangeDutyCycle(2.5)
+     time.sleep(0.5)
+
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
