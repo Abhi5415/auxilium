b8a4729f2fff9bc6d8fa3b2b25f0a43394ca002c
Abhinav <abhinav.b5415@gmail.com>
2019-09-07 23:27:17 -0400

    abhi5415 - exported isExisting

--- auxilium-server/controllers/facial.js
+++ auxilium-server/controllers/facial.js
@@ -28,7 +28,7 @@ let getObjects = () => {
 let isSameUser = (source, target) => {
 	return new Promise(resolve => {
 		if (source === target) {
-			resolve(false);
+			resolve({ exists: false, matchingFace: '' });
 			return;
 		}
 		let params = {
@@ -51,19 +51,16 @@ let isSameUser = (source, target) => {
 			if (err) console.log(err, err.stack);
 			else {
 				if (data.FaceMatches.length > 0) {
-					console.log(source);
-					console.log(data);
-					resolve(true);
+					resolve({ exists: true, matchingFace: target });
 				} else {
-					resolve(false);
+					resolve({ exists: false, matchingFace: '' });
 				}
 			} // successful response
 		});
 	});
 };
 
-let isExistingUser = async sourceImageS3 => {
-	console.log('here');
+exports.isExistingUser = async sourceImageS3 => {
 	let data = await getObjects();
 
 	let promises = [];
@@ -74,62 +71,12 @@ let isExistingUser = async sourceImageS3 => {
 
 	let res = await Promise.all(promises);
 
-	if (res.includes(true)) return true;
-	else return false;
+	let result = res.find(datum => datum.exists);
+	if (result === undefined) return { exists: false, matchingFace: '' };
+	else {
+		result.matchingFace = `https://facial-data.s3.amazonaws.com/${result.matchingFace}`;
+		return result;
+	}
 };
 
-isExistingUser('IMG_20190826_170830.jpg').then(res => console.log(res));
-
-// let bucket_params = {
-// 	Bucket: 'facial-data',
-// 	MaxKeys: 100,
-// };
-
-// let registeredUsers = [];
-
-// s3.listObjects(bucket_params, function(err, data) {
-// 	console.log(data);
-// 	if (err) console.log(err, err.stack);
-// 	// an error occurred
-// 	else {
-// 		console.log('------ The S3 Contents -------');
-// 		console.log(data.Contents);
-// 		registeredUsers = data.Contents;
-
-// 		registeredUsers.forEach(user => {
-// 			if (imageSourceS3 !== user.Key) {
-// 				let params = {
-// 					SimilarityThreshold: 90,
-// 					SourceImage: {
-// 						S3Object: {
-// 							Bucket: 'facial-data',
-// 							Name: imageSourceS3,
-// 						},
-// 					},
-// 					TargetImage: {
-// 						S3Object: {
-// 							Bucket: 'facial-data',
-// 							Name: user.Key,
-// 						},
-// 					},
-// 				};
-
-// 				rekognition.compareFaces(params, function(err, data) {
-// 					if (err) console.log(err, err.stack);
-// 					// an error occurred
-// 					else {
-// 						console.log('------ Match Data -------');
-// 						console.log(data);
-// 						if (data.FaceMatches.length > 0) {
-// 							console.log('------ The Match -------');
-// 							console.log(user.Key);
-// 							return false;
-// 						}
-// 					} // successful response
-// 				});
-// 			}
-// 		});
-// 	} // successful response
-// });
-
-// isExistingUser('IMG_20190826_170830.jpg').then(res => console.log(res));
+isExistingUser('sherry1.jpg').then(res => console.log(res));
