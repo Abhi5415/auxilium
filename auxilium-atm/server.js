e5c465f0d533a3fa05574a3d1734b07a8ad5e1b3
Mufeez Amjad <mufeez.amjad@outlook.com>
2019-09-08 00:03:11 -0400

    close files after opening, continuous read of file

--- auxilium-atm/server.js
+++ auxilium-atm/server.js
@@ -14,10 +14,13 @@ app.get('/api/atm/deposit', (req, res) => {
 		fs.readFile('./deposit.txt', 'utf8', (err, contents) => {
 			if (contents != 'pending') {
 				console.log('complete');
-				res.status(200).send(contents);
+				res.status(200).send({
+					count: parseInt(contents)
+				});
 				return;
 			}
 		});
+		fs.close()
 		setTimeout(() => { }, 5000);
 	}
 });
@@ -27,14 +30,17 @@ app.post('/api/atm/withdraw', (req, res) => {
 
 	if (rotations) {
 		shell.exec(`python servo.py ${rotations}`);
-
-		fs.readFile('./withdraw.txt', 'utf8', (err, contents) => {
-			if (contents != 'pending') {
-				console.log('complete');
-				res.status(202).send();
-				return;
-			}
-		});
+		while (true) {
+			fs.readFile('./withdraw.txt', 'utf8', (err, contents) => {
+				if (contents != 'pending') {
+					console.log('complete');
+					res.status(202).send();
+					return;
+				}
+			});
+			fs.close();
+			setTimeout(() => { }, 5000);
+		}
 	} else {
 		res.status(400).send();
 	}
